a06c1139f9467ff0339a0647c2fdd4df752519ef
Willy DONGMO <w.dongmo@gmail.com>
2016-04-19 20:23:43 +0200

    chore

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -2,11 +2,6 @@
 class AppController {
 	constructor(){
 		this.menuItems = ['Leboncheveu','catalogue','Blog','Magasin','Connexion/Inscription'];
-		this.isTrue = false;
-		this.toggle = function (){
-			console.log('item clicked','isTrue ',this.isTrue);
-			return this.isTrue =!this.isTrue;
-		}
 	}
 }
 
--- client/app/components/banniere/banniere.controller.js
+++ client/app/components/banniere/banniere.controller.js
@@ -1,17 +1,20 @@
 
 class BanniereController {
   constructor() {
-		  this.myInterval = 5000;
+		  this.myInterval = 1000000;
 		  this.noWrapSlides = false;
 		  this.active = 0;
 		  var slides = this.slides = [];
 		  var currIndex = 0;
 
 		  this.addSlide = function() {
-		    var newWidth = 900 + slides.length + 1;
+		    var newWidth = 1280 + slides.length + 1;
 		    slides.push({
 		      image: 'http://lorempixel.com/' + newWidth + '/400',
-		      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
+		      text: ['Un style toujours dans l\'air du temps à des coûts défiants toutes concurences ...',
+		      		 'Pas besoin de vous déplacer ni de prévoir du temps pour vos chercher le nécessaires pours vos coiffures ...',
+		      		 ,'Leboncheveu est le choix gagnant de nombreuses femmes ...',
+		      		 'Nos coiffeuses vous donnerons toujours le meilleur d\'elle même ...'][slides.length % 4],
 		      id: currIndex++
 		    });
 		  };
