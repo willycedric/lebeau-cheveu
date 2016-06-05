57d74e50dea8c109716b20993abeb94a1573944b
Willy DONGMO <w.dongmo@gmail.com>
2016-06-05 15:00:18 +0200

    chore step-2

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -1,10 +1,36 @@
 class LoginController {
-  constructor() {
+  constructor($http) {
   	//labels
-    this.greeting = 'LoginController!';
+    this.user = {email:"none",password:"none"};
+    console.log(this.user.email);
+    this.login = function(data){
+    	$http({
+    		data,
+    		url:'http://localhost:3000/api/users/me',
+    		method:'POST'
+
+    	}).then(function successCallBack(response){
+    		console.log(JSON.stringify(response.data));
+    	}, function errorCallBack(err){
+    		console.error(err);
+    	});
+    };
+
+    this.getMe = function(){
+        $http({
+            url:'http://localhost:3000/api/users/me',
+            method:'GET'
+        }).then(function successCallBack(response){
+            console.log(JSON.stringify(response.data));
+        }, function errorCallBack(err){
+            console.error(err);
+        });
+    };
   }
 
 }
+
+LoginController.$inject = ['$http'];
 
 export {LoginController};
 
--- client/app/components/login/login.directive.js
+++ client/app/components/login/login.directive.js
@@ -1,6 +1,7 @@
 import './login.css';
 import {LoginController as controller} from './login.controller';
 import template from './login.html';
+//import $ from 'jquery';
 
 export const loginDirective = ()=> {
 	 return{
