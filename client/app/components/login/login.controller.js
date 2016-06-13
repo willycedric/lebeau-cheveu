6fcfc358440a71ba039cc2a84d4daee1d10b8797
Willy DONGMO <w.dongmo@gmail.com>
2016-06-14 01:22:00 +0200

    chore step-3

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -1,20 +1,23 @@
 class LoginController {
   constructor($http) {
+    this.token=null;
+    
   	//labels
     this.user = {email:"none",password:"none"};
     console.log(this.user.email);
     this.login = function(data){
+        console.log(data);
     	$http({
     		data,
     		url:'http://localhost:3000/api/users/me',
     		method:'POST'
-
     	}).then(function successCallBack(response){
-    		console.log(JSON.stringify(response.data));
+            $http.defaults.headers.common['Authorization'] = 'JWT '+ response.data.token;
     	}, function errorCallBack(err){
     		console.error(err);
     	});
     };
+    console.log("token received ", this.token);
 
     this.getMe = function(){
         $http({
