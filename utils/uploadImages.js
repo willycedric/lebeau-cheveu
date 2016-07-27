410bbe30e8328964795f768f934c5feaa7c55e97
Willy DONGMO <w.dongmo@gmail.com>
2016-07-27 20:32:52 +0200

    chore master

--- utils/uploadImages.js
+++ utils/uploadImages.js
@@ -3,24 +3,6 @@ var config = require('./config/config')
 ,fs= require('fs'),
 Promise =require('bluebird');
 
-/*var uploadImage = function(){
-	
-	var images=['banner1.jpg','banner5.jpg','banner8.jpg','banner6.jpg','banner7.jpg'];
-	var imageDetails=[];
-    images.forEach(function(elt){
-	    cloudinary.uploader.upload(__dirname+'/utils/images/'+elt,function(err,result){
-	        imagesDetails.push({
-	          url: result.secure_utl,
-	          width:result.width,
-	          height:result.height,
-	          create_at:result.create_at
-	        });
-	     });
-	  });
-
-};*/
-
-
 module.exports = function(elt){
 	cloudinary.config(require('./config/config').cloudinary);
 	return new Promise(function(resolve,reject){
@@ -33,13 +15,3 @@ module.exports = function(elt){
 		});
 	});
 };
-
-/*module.exports = function(){
-	return new Promise(function(resole,reject){
-			if(uploadImage(fileName)){
-				resolve(uploadImage(fileName));
-			}else{
-				reject(new Error(" file "+fileName+" can't be loaded"));
-			}
-	});
-};*/
