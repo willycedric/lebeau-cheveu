31a6c301adaf2faf4d34ded8d719dc7e7c126ab5
Willy DONGMO <w.dongmo@gmail.com>
2016-08-03 23:12:53 +0200

    chore master

--- client/app/shared/api.js
+++ client/app/shared/api.js
@@ -1,6 +1,6 @@
 export const api = {
-  url: 'https://pacific-taiga-58505.herokuapp.com',
-  homeUrl:'http://localhost:3000',
+  url: 'http://localhost:3000',
+  homeUrl:'https://pacific-taiga-58505.herokuapp.com',
   home:'http://localhost:4500/#/home',
   error:'http://localhost:4500/#/error'
 };
