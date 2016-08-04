abd4885b20b52a920999e2e54309d1bef924fd2a
Willy DONGMO <w.dongmo@gmail.com>
2016-08-04 21:43:39 +0200

    chore master

--- client/app/shared/api.js
+++ client/app/shared/api.js
@@ -1,6 +1,6 @@
 export const api = {
   url: 'http://localhost:3000',
-  homeUrl:'https://lebeaucheveu-bachend.herokuapp.com/',
+  homeUrl:'https://lebeaucheveu-bachend.herokuapp.com',
   home:'https://lebeaucheveu.herokuapp.com/#/home',
   error:'https://lebeaucheveu.herokuapp.com/#/error'
 };
