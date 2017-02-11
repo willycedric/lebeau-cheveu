72f1bde516ec1d437bcc758989393f9fd569cb8d
Willy DONGMO <w.dongmo@gmail.com>
2017-02-11 11:03:33 +0100

    chore

--- client/app/components/blog/blog.controller.js
+++ client/app/components/blog/blog.controller.js
@@ -1,11 +1,37 @@
 class BlogController {
-  constructor() {
-  	//labels
-    this.firstName="Willy";
-    this.lastName="Dongmo";
+  constructor($http) {
+  	 //console.log(this.createPost(this.post));
+  	 this.$http=$http;
+  	 this.init();
+  }
+
+  /**
+   * [createPost description]
+   * @param  {[type]} post [description]
+   * @return {[type]}      [description]
+   */
+  createPost(post){
+  	this.$http.post("http://localhost:3000/api/posts/",post)
+  	.success((resp)=>{
+  		this.getAllPosts();
+  	});
+  }
+
+  /**
+   * [getAllPosts description]
+   * @return {[type]} [description]
+   */
+  getAllPosts(){
+  	this.$http.get("http://localhost:3000/api/posts/")
+  	.success(this.getAllPosts());
+  }
+
+  init(){
+  	this.getAllPosts();
   }
 
 }
+BlogController.$inject =['$http'];
 
 export {BlogController};
 
